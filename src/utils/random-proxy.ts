e3a3a51e27ea8b89fba7a34b5f60d927ed305d48
1CEs <isongwut.me@gmail.com>
2025-04-13 16:43:04 +0700

    chore: implemented random proxy api

--- src/utils/random-proxy.ts
+++ src/utils/random-proxy.ts
@@ -1,8 +1,35 @@
-import fs from 'fs';
-import path from 'path';
+import { proxyCountry } from "../data/country";
 
-export function getRandomProxy() {
-  const proxies = fs.readFileSync(path.join(__dirname, '../data/proxies.txt'), 'utf8').split('\n');
-  const randomProxy = proxies[Math.floor(Math.random() * proxies.length)];
-  return randomProxy;
-};
+export const randomProxy = async () => {
+    while (true) {
+        const randomCountry = proxyCountry[Math.floor(Math.random() * proxyCountry.length)];
+        const proxyEndpoint = process.env.PROXY_API_ENDPOINT;
+        if (!proxyEndpoint) {
+            throw new Error("PROXY_API_ENDPOINT is not set");
+        }
+
+        const detailURL = `${proxyEndpoint}/proxies?country=${randomCountry}&ui=true&lang=en`
+        const detailResponse = await fetch(detailURL);
+        const data = await detailResponse.json() as CountryDetail;
+        const proxyList: ProxyOfCountry[] = data.proxies;
+        const isProxyCanPing = proxyList.filter((proxy) => proxy.ping != null);
+
+        if(isProxyCanPing.length == 0) {
+            continue;
+        }
+
+        const randomProxy = isProxyCanPing[Math.floor(Math.random() * isProxyCanPing.length)];
+
+        if (!randomProxy) {
+            continue;
+        }
+        const proxyURL = `${proxyEndpoint}/proxy?country=${randomCountry}&ui=true&lang=en&hostname=${randomProxy?.hostname}&port=${randomProxy.port}`
+        const credential = await fetch(proxyURL);
+        const credentialData = await credential.json() as any;
+        return {
+            proxy: `${credentialData.proxy.hostname}:${credentialData.proxy.port}`,
+            username: credentialData.proxy.login,
+            password: credentialData.proxy.password
+        }
+    }
+}
